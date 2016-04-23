968a702ac9a05b12f507a4fdd9d76419a1781236
Joshua Echeverri <josheche@gmail.com>
2016-04-23 12:11:48 -0400

    root route postsindex when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts 
+  resources :posts
   resources :users
+  root to: 'posts#index', constraints: lambda{|req| !req.session[:user_id].blank?}
   root to: 'visitors#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
